e78f1d96b5f3b25b20c46923b65f4bcc92e1db55
gcadorette <cadorette.guillaume@gmail.com>
2024-07-01 18:10:11 -0400

    nvm now the photos are saved in db

--- backend/models/photomodel.py
+++ backend/models/photomodel.py
@@ -3,6 +3,11 @@ class Photo:
     url = ""
     thumbnail_url = ""
     insert_date = None
+    def __init__(self, url, thumbnail_url):
+        self.id = ""
+        self.url = url
+        self.thumbnail_url = thumbnail_url
+        self.insert_date = None
 
 class Photo_Click:
     def __init__(self, photo_id):
--- backend/routes/admin.py
+++ backend/routes/admin.py
@@ -1,5 +1,7 @@
 from backend.router import app
 from backend import constants
+from backend.client import mongodbclient
+from backend.models.photomodel import Photo
 
 from flask import Flask, flash, request, redirect, url_for
 from werkzeug.utils import secure_filename
@@ -14,18 +16,21 @@ def upload_file():
         return "No files selected", 400
     uploaded_files = []
     invalid_files = []
+    photos_to_save = []
     for file in files:
         filename = secure_filename(file.filename)
         if filename.rsplit(".", 1)[1].upper() in constants.ALLOWED_FILES:
             full_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
             file.save(full_path)
-            
+            thumbnail_url = f'{os.path.join(app.config["UPLOAD_FOLDER"], constants.THUMBNAILS_DIR, filename)}.{constants.THUMBNAIL_FORMAT}'
             with Image.open(full_path) as im:
                 im.thumbnail(constants.THUMBNAIL_SIZE)
-                im.save(os.path.join(app.config['UPLOAD_FOLDER'], constants.THUMBNAILS_DIR, filename), "JPEG")
+                im.save(thumbnail_url)
             uploaded_files.append(filename)
+            photos_to_save.append(Photo(full_path, thumbnail_url))
         else:
             invalid_files.append(filename)
+    mongodbclient.insert_photos(photos_to_save)
     return {
         "invalid_files": invalid_files,
         "uploaded_files": uploaded_files
